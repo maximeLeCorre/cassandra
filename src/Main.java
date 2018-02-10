e6e5e1180c36acb1baa942aaf13a37dc6ae5cedc
Portable_Maxime <maxime.lecorre96@gmail.com>
2018-02-10 15:42:47 +0100

    Version finale

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Main {
@@ -10,7 +18,6 @@ public class Main {
     private static Session session;
     static Integer port = 9042;
     static String node = "127.0.0.1";
-
 
     public static void main(String[] args) {
 
@@ -23,6 +30,87 @@ public class Main {
 
         session = cluster.connect();
 
-        session.execute("DROP TABLE IF EXISTS school.lesson;");
+        JSONParser parser = new JSONParser();
+
+
+        try{
+            Object obj = parser.parse(new FileReader("./stocks.json"));
+
+            JSONArray jsonArray = (JSONArray) obj;
+
+            //On boucle sur le json
+            for(int i = 0; jsonArray.size() > i; i++){
+                JSONObject toInsert = (JSONObject) jsonArray.get(i);
+
+                String query = "INSERT INTO stockexchange.stock (" +
+                        "\"20-day\"," +
+                        "\"200-day\"," +
+                        "\"50-day\"," +
+                        "\"52-week\"," +
+                        "analystrecom," +
+                        "averagetruerange," +
+                        "averagevolume," +
+                        "beta," +
+                        "change," +
+                        "company," +
+                        "epsttm," +
+                        "earningdate," +
+                        "price," +
+                        "roi," +
+                        "id," +
+                        "description," +
+                        "performance," +
+                        "ratio)" +
+                        "VALUES (";
+
+                        String value = toInsert.get("20-Day Simple Moving Average") + ",";
+                        value += toInsert.get("200-Day Simple Moving Average") + ",";
+                        value += toInsert.get("50-Day") + ",";
+                        value += toInsert.get("52-Week") + ",";
+                        value +=toInsert.get("Analyst Recom") + ",";
+                        value +=toInsert.get("Average True Range") + ",";
+                        value +=toInsert.get("Average Volume") + "," ;
+                        value +=toInsert.get("Beta") + ",";
+                        value +=toInsert.get("Change") + ",";
+                        value = value.replace("null", "0");
+
+                        String company = (String) toInsert.get("Company");
+                        value +="'" + company.replace("'", "-") + "',";
+
+                        value = value.replace("null" , "'NULL'");
+                        value +=toInsert.get("EPS ttm") + ",";
+                        value = value.replace("null", "0");
+                        value +=toInsert.get("Earnings Date") + ",";
+                        value = value.replace("null", "{'$date': 'null'}");
+                        value +=toInsert.get("Price") + ",";
+                        value +=toInsert.get("ROI") + ",";
+                        value = value.replaceAll("null" , "0");
+                        value +=toInsert.get("_id") + ",";
+                        value = value.replaceAll("null" , "'NULL'");
+                        value +=toInsert.get("description") + ",";
+                        value = value.replaceAll("null" , "'NULL'");
+                        value +=toInsert.get("performance") + ",";
+                        value +=toInsert.get("ratio") + ")";
+                        value = value.replaceAll("null" , "0");
+
+
+                value = value.replace('\"', '\'');
+
+                query += value;
+                System.out.println(query);
+                session.execute(query);
+            }
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        //session.execute("DROP TABLE IF EXISTS school.lesson;");
+        session.close();
+
     }
 }
